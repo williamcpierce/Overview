191841ccd949ea6fa6fc96a66e4a23040ad435d6
William Pierce <wpierce118@gmail.com>
2024-12-08 00:16:49 -0800

    Simplified InteractionView code

--- Overview/Preview/InteractionView.swift
+++ Overview/Preview/InteractionView.swift
@@ -14,16 +14,17 @@
 import SwiftUI
 
 struct InteractionOverlay: NSViewRepresentable {
-    // MARK: - Properties
     @Binding var isEditModeEnabled: Bool
     let isBringToFrontEnabled: Bool
     let bringToFrontAction: () -> Void
     let toggleEditModeAction: () -> Void
     
-    // MARK: - NSViewRepresentable Methods
     func makeNSView(context: Context) -> NSView {
         let view = InteractionView()
-        configureView(view)
+        view.isEditModeEnabled = isEditModeEnabled
+        view.isBringToFrontEnabled = isBringToFrontEnabled
+        view.bringToFrontAction = bringToFrontAction
+        view.toggleEditModeAction = toggleEditModeAction
         view.menu = createContextMenu(for: view)
         return view
     }
@@ -31,77 +32,44 @@ struct InteractionOverlay: NSViewRepresentable {
     func updateNSView(_ nsView: NSView, context: Context) {
         guard let view = nsView as? InteractionView else { return }
         view.isEditModeEnabled = isEditModeEnabled
-        view.updateEditModeMenuItem()
-    }
-    
-    // MARK: - Private Methods
-    private func configureView(_ view: InteractionView) {
-        view.isEditModeEnabled = isEditModeEnabled
-        view.isBringToFrontEnabled = isBringToFrontEnabled
-        view.bringToFrontAction = bringToFrontAction
-        view.toggleEditModeAction = toggleEditModeAction
+        view.editModeMenuItem?.state = isEditModeEnabled ? .on : .off
     }
     
     private func createContextMenu(for view: InteractionView) -> NSMenu {
         let menu = NSMenu()
         
-        // Add Edit Mode item
         let editModeItem = NSMenuItem(
             title: "Edit Mode",
-            action: #selector(InteractionView.toggleEditMode(_:)),
+            action: #selector(InteractionView.toggleEditMode),
             keyEquivalent: ""
         )
         editModeItem.target = view
         menu.addItem(editModeItem)
         
-        // Add separator
         menu.addItem(NSMenuItem.separator())
-        
-        // Add Close Window item
-        let closeItem = NSMenuItem(
-            title: "Close Window",
-            action: #selector(NSWindow.close),
-            keyEquivalent: ""
-        )
-        menu.addItem(closeItem)
+        menu.addItem(NSMenuItem(title: "Close Window", action: #selector(NSWindow.close), keyEquivalent: ""))
         
         view.editModeMenuItem = editModeItem
         return menu
     }
 }
 
-// MARK: - InteractionView Implementation
-extension InteractionOverlay {
-    final class InteractionView: NSView {
-        // MARK: - Properties
-        var isEditModeEnabled = false {
-            didSet { updateEditModeMenuItem() }
+private final class InteractionView: NSView {
+    var isEditModeEnabled = false
+    var isBringToFrontEnabled = false
+    var bringToFrontAction: (() -> Void)?
+    var toggleEditModeAction: (() -> Void)?
+    weak var editModeMenuItem: NSMenuItem?
+    
+    override func mouseDown(with event: NSEvent) {
+        if !isEditModeEnabled && isBringToFrontEnabled {
+            bringToFrontAction?()
+        } else {
+            super.mouseDown(with: event)
         }
-        var isBringToFrontEnabled: Bool = false
-        var bringToFrontAction: (() -> Void)?
-        var toggleEditModeAction: (() -> Void)?
-        weak var editModeMenuItem: NSMenuItem?
-        
-        // MARK: - Mouse Event Handling
-        override func mouseDown(with event: NSEvent) {
-            if !isEditModeEnabled && isBringToFrontEnabled {
-                bringToFrontAction?()
-            } else {
-                super.mouseDown(with: event)
-            }
-        }
-        
-        override func rightMouseDown(with event: NSEvent) {
-            super.rightMouseDown(with: event)
-        }
-        
-        // MARK: - Actions
-        @objc func toggleEditMode(_ sender: Any?) {
-            toggleEditModeAction?()
-        }
-        
-        func updateEditModeMenuItem() {
-            editModeMenuItem?.state = isEditModeEnabled ? .on : .off
-        }
+    }
+    
+    @objc func toggleEditMode() {
+        toggleEditModeAction?()
     }
 }
