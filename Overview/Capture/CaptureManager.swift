ee077c73374b71b662139930f84361bef9d54010
Imogen-Pierce <epierce118@gmail.com>
2025-01-20 21:35:21 -0500

    modified synchronizeSourceTitle

--- Overview/Capture/CaptureManager.swift
+++ Overview/Capture/CaptureManager.swift
@@ -169,6 +169,7 @@ final class CaptureManager: ObservableObject {
 
         let sourceID = SourceManager.SourceID(processID: processID, windowID: source.windowID)
         sourceWindowTitle = titles[sourceID]
+        sourceApplicationTitle = source.owningApplication?.applicationName
     }
 
     private func synchronizeStreamConfiguration() async {
