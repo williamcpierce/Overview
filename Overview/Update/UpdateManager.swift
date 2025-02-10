ddedd0713b8a758cb0d51f857ec53971ac98a96d
William Pierce <wpierce118@gmail.com>
2025-02-10 02:37:58 -0800

    Minor refactor

--- Overview/Update/UpdateManager.swift
+++ Overview/Update/UpdateManager.swift
@@ -3,6 +3,9 @@
  Overview
 
  Created by William Pierce on 2/10/25.
+
+ Coordinates update checking and status tracking through the Sparkle
+ framework integration.
 */
 
 import Sparkle
@@ -12,25 +15,31 @@ import SwiftUI
 final class UpdateManager: ObservableObject {
     // Dependencies
     private let logger = AppLogger.settings
+    private let updaterController: SPUStandardUpdaterController
 
     // Published State
-    @Published var updater: SPUUpdater
-    @Published var canCheckForUpdates: Bool = false
-
-    // Private State
-    let updaterController: SPUStandardUpdaterController
+    @Published private(set) var updater: SPUUpdater
+    @Published private(set) var canCheckForUpdates: Bool = false
 
     init() {
-        self.updaterController = SPUStandardUpdaterController(
+        logger.debug("Initializing update manager")
+
+        // Initialize the updater controller first
+        updaterController = SPUStandardUpdaterController(
             startingUpdater: true,
             updaterDelegate: nil,
             userDriverDelegate: nil
         )
-        self.updater = updaterController.updater
-        self.updater
-            .publisher(for: \.canCheckForUpdates)
+        updater = updaterController.updater
+
+        // Configure update status binding
+        updater.publisher(for: \.canCheckForUpdates)
             .assign(to: &$canCheckForUpdates)
+
+        logger.debug("Update manager initialization complete")
     }
+
+    // MARK: - Public Methods
 
     func checkForUpdates() {
         logger.info("Initiating update check")
